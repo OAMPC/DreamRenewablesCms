6c382f0d3feddebb59a8753c97015e88906877b9
popey2700 <alexanderpope27@outlook.com>
2024-07-05 00:25:07 +0100

    Provision heroku DB for Strapi

--- dream-renewables-terraform/variables.tf
+++ dream-renewables-terraform/variables.tf
@@ -2,3 +2,8 @@ variable "heroku_cms_app_name" {
   description = "Name of the Heroku app"
   default     = "dream-renewables-strapi"
 }
+
+variable "heroku_cms_db_name" {
+  description = "Name of the Heroku app db"
+  default     = "dream-renewables-strapi-postgres-db"
+}
