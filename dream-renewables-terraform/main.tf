eb7a62543b065499e526520cc2fb07675066b54a
popey2700 <alexanderpope27@outlook.com>
2024-07-04 23:58:26 +0100

    Create Heroku Strapi App

--- dream-renewables-terraform/main.tf
+++ dream-renewables-terraform/main.tf
@@ -6,3 +6,8 @@ terraform {
     }
   }
 }
+
+resource "heroku_app" "dream_renewables_strapi" {
+  name   = var.heroku_cms_app_name
+  region = "eu"
+}
---
+++ dream-renewables-terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "heroku_cms_app_name" {
+  description = "Name of the Heroku app"
+  default     = "dream-renewables-strapi"
+}
