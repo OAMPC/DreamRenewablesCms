d49dd0461ed85c57e5a586e6c27c8e173e30e3a6
popey2700 <alex.pope@madetech.com>
2024-08-09 11:02:39 +0100

    introduce simple hello world

---
+++ dream-renewables-strapi/src/api/hello-world/controllers/hello-world.ts
@@ -0,0 +1,7 @@
+/**
+ * hello-world controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::hello-world.hello-world');
---
+++ dream-renewables-strapi/src/api/hello-world/services/hello-world.ts
@@ -0,0 +1,7 @@
+/**
+ * hello-world service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::hello-world.hello-world');
