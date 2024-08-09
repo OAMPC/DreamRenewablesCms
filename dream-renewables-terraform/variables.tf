4ea83a596c46129e3babe431db568cc6a6fb123a
Alex Pope <89132764+popey2700@users.noreply.github.com>
2024-08-09 18:57:14 +0100

    Remove Terraform (#6)
    * Removed Terraform
    
    * Refactor project directories

--- dream-renewables-terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "heroku_cms_app_name" {
-  description = "Name of the Heroku app"
-  default     = "dream-renewables-strapi"
-}
-
-variable "heroku_cms_db_name" {
-  description = "Name of the Heroku app db"
-  default     = "dream-renewables-strapi-postgres-db"
-}
