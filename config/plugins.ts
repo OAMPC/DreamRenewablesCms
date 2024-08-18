4c4b443f2448b6ce8a21c34b0f033d7c264c96cb
popey2700 <alexanderpope27@outlook.com>
2024-08-18 20:17:44 +0100

    Fix media upload bug

--- config/plugins.ts
+++ config/plugins.ts
@@ -4,10 +4,10 @@ export default ({ env }) => ({
       provider: "aws-s3",
       providerOptions: {
         accessKeyId: env("AWS_ACCESS_KEY_ID"),
-        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        secretAccessKey: env("AWS_SECRET_ACCESS_KEY"),
         region: env("AWS_REGION"),
         params: {
-          ACL: env("AWS_ACL", "public-read"),
+          ACL: env("AWS_ACL", "private"),
           signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
           Bucket: env("AWS_BUCKET"),
         },
