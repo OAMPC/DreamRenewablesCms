7530f7406e95b9fa5c0c5e3f9f18336fdded8aa7
Alex Pope <89132764+popey2700@users.noreply.github.com>
2024-08-19 09:55:08 +0100

    Create Navbar types (#10)

--- src/api/hello-world/services/hello-world.ts
+++
@@ -1,7 +0,0 @@
-/**
- * hello-world service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::hello-world.hello-world');
--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,47 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface NavigationBarButton extends Schema.Component {
+  collectionName: 'components_navigation_bar_buttons';
+  info: {
+    displayName: 'Button';
+  };
+  attributes: {
+    buttonString: Attribute.String;
+    buttonSlug: Attribute.String;
+  };
+}
+
+export interface NavigationBarDropDownLinks extends Schema.Component {
+  collectionName: 'components_navigation_bar_drop_down_links';
+  info: {
+    displayName: 'Drop Down Link';
+    description: '';
+  };
+  attributes: {
+    dropDownLinkString: Attribute.String;
+    dropDownLinkSlug: Attribute.String;
+    dropDownLinks: Attribute.Component<'navigation-bar.link', true>;
+  };
+}
+
+export interface NavigationBarLink extends Schema.Component {
+  collectionName: 'components_navigation_bar_links';
+  info: {
+    displayName: 'Standard Link';
+    description: '';
+  };
+  attributes: {
+    linkString: Attribute.String;
+    linkSlug: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'navigation-bar.button': NavigationBarButton;
+      'navigation-bar.drop-down-links': NavigationBarDropDownLinks;
+      'navigation-bar.link': NavigationBarLink;
+    }
+  }
 }
