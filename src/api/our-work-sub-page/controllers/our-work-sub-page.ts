75559fbde83cb681a1bfd1f0b5c1a38e61a26387
12OMarsden <40914332+12OMarsden@users.noreply.github.com>
2025-01-19 13:58:58 +0000

    Create initial types

---
+++ src/api/our-work-sub-page/controllers/our-work-sub-page.ts
@@ -0,0 +1,7 @@
+/**
+ * our-work-sub-page controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::our-work-sub-page.our-work-sub-page');
---
+++ src/api/our-work-sub-page/routes/our-work-sub-page.ts
@@ -0,0 +1,7 @@
+/**
+ * our-work-sub-page router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::our-work-sub-page.our-work-sub-page');
