d4f6c9bb2b5649e9488c324cd9145f651dfd273d
popey2700 <alexanderpope27@outlook.com>
2025-05-28 19:59:37 +0100

    hackily implement philantropy page using blog template

---
+++ src/api/philanthropy-page/controllers/philanthropy-page.ts
@@ -0,0 +1,7 @@
+/**
+ * philanthropy-page controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::philanthropy-page.philanthropy-page');
---
+++ src/api/philanthropy-page/routes/philanthropy-page.ts
@@ -0,0 +1,7 @@
+/**
+ * philanthropy-page router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::philanthropy-page.philanthropy-page');
