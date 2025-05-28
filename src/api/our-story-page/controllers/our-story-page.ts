a0e3bb2147fc6210e2dec2dd3ba10b6d355c8cf4
Alex Pope <89132764+popey2700@users.noreply.github.com>
2025-05-28 19:39:53 +0100

    Hacky implementation of blog post for our story page (#34)

---
+++ src/api/our-story-page/controllers/our-story-page.ts
@@ -0,0 +1,7 @@
+/**
+ * our-story-page controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::our-story-page.our-story-page');
---
+++ src/api/our-story-page/services/our-story-page.ts
@@ -0,0 +1,7 @@
+/**
+ * our-story-page service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::our-story-page.our-story-page');
